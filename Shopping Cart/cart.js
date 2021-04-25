984867dd54b22828f718afe22e4ae77b508b235f
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2021-04-25 21:43:37 +0900

    cart기능 일부 구현

--- Shopping Cart/cart.js
+++ Shopping Cart/cart.js
@@ -1,117 +1,152 @@
-const carts = [];
+// data 배열로 담음
+let cartItems = [];
+const $cartLiTemplate = document.getElementById('cart-item-li').content
+  .firstElementChild;
 
 // DOM
-const $goListPage = document.querySelector('.goListPage');
+// const $goListPage = document.querySelector('.goListPage');
 const $allSelect = document.querySelector('.allSelect');
 const $allDelete = document.querySelector('.allDelete');
 const $itemCheckBox = document.querySelector('.itemCheckBox');
-const $cartItemDel = document.querySelector('.cartItemDel');
-// const $eachItemList = document.querySelector('.eachItemList');
+const $cartItemLists = document.querySelector('.cartItemLists');
+const $cartTotalCost = document.querySelector('.cartTotalCost');
+// const $cartCountNum = document.querySelector('.cartCountNum');
+const $totalCost = document.querySelector('.totalCost');
 
-// 목록 페이지로 가는 함수
-$goListPage.onclick = async () => {
-  // window.location.href = './list.html';
-};
+// 금액 자리수
+// const moneyfilter = money => {
+//   return '₩' + money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+// };
 
-const CartNumber = async e => {
+//-------
+// 카트에 담긴 상품들 랜더링.
+document.addEventListener('DOMContentLoaded', async () => {
+  const fetchItems = async () => {
+    try {
+      const res = await fetch('http://localhost:4000/api/appPosts/mycart');
+      cartItems = await res.json();
+      console.log(cartItems, '카트배열');
+    } catch (e) {
+      console.log(e, '카트에러남ㅋ');
+    }
+  };
+  await fetchItems();
+
+  const $CartFragmentNode = document.createDocumentFragment();
+
+  cartItems.forEach(item => {
+    const $newCartNode = $cartLiTemplate.cloneNode(true);
+    // 데이터가 바뀌는 노드만 잡기.
+    const $cartImg = $newCartNode.querySelector('.cartImg');
+    const $cartProductCoffeeName = $newCartNode.querySelector(
+      '.cartProductCoffeeName'
+    );
+    const $cartCoffeeType = $newCartNode.querySelector('.cartCoffeeType');
+    const $cartEachCoffeeInfo = $newCartNode.querySelector(
+      '.cartEachCoffeeInfo'
+    );
+    const $cartCountNum = $newCartNode.querySelector('.cartCountNum');
+    const $cartTotalCost = $newCartNode.querySelector('.cartTotalCost');
+
+    $newCartNode.classList.add(item.cartId);
+    $newCartNode.id = item._id;
+    console.log($newCartNode, 'jioif');
+    // $imgNode.src = item.imageUrl;
+    $cartImg.setAttribute('src', item.imageUrl);
+    $cartProductCoffeeName.textContent = item.name;
+    $cartCoffeeType.textContent = item.type;
+    $cartEachCoffeeInfo.textContent = item.subName;
+    $cartCountNum.textContent = item.numberOfCart;
+    $cartTotalCost.textContent = item.price * $cartCountNum.textContent;
+    // ₩
+
+    $CartFragmentNode.appendChild($newCartNode);
+  });
+  $cartItemLists.appendChild($CartFragmentNode);
+
+  const allCosts = () => {
+    if (!cartItems.length) return '';
+    return cartItems.reduce(
+      (acc, cur) => acc + cur.price * cur.numberOfCart,
+      0
+    );
+  };
+
+  $totalCost.textContent = allCosts();
+});
+
+// 리스트 추가 감소.
+const changeCartNumber = async e => {
+  if (!(e.target.matches('.cartDownBtn') || e.target.matches('.cartUpBtn')))
+    return;
+
   const $targetLi = e.target.closest('li');
-  const $number = $targetLi.querySelector('.cart-item-number');
-  const $increase = $targetLi.querySelector('.cart-item-increase');
-  const $decrease = $targetLi.querySelector('.cart-item-decrease');
-  const cartId = $targetLi.classList[1];
+  const $number = $targetLi.querySelector('.cartCountNum');
+  const $downBtn = $targetLi.querySelector('.cartDownBtn');
+  const $upBtn = $targetLi.querySelector('.cartUpBtn');
+  const $cost = $targetLi.querySelector('.cartTotalCost');
+  const cartId = $targetLi.id;
+  console.log(cartId, 49305);
   const number = +$number.textContent;
-  if (+$number.textContent > 1) {
-    const res = await fetch(`http://localhost:4000/api/mycart/${cartId}`, {
-      method: 'PATCH',
-      headers: 'Content-Type:application/json',
-      body: JSON.stringify({ numberOfCart: +$number.textContent })
-    });
-    const res2 = await res.json();
-    console.log(res2);
-  }
-};
-
-// 모든 체크박스 체크하기.
-$itemCheckBox.onchange = e => {
-  console.log(e.target, 123);
-  try {
-    // const res = await fetch('/todos', {
-    //   method: 'PATCH',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ completed: $ckCompleteAll.checked })
-    // });
-    // todos = await res.json();
-    // render();
-  } catch (err) {
-    console.error('[ERROR!!]', err);
-  }
-};
-
-// 체크된 리스트 삭제
-$allDelete.onclick = e => {
-  const $eachItemList = document.querySelector('.eachItemList');
-  console.log(e.target, 454);
-  console.log($eachItemList, 23);
-  try {
-    // const res = await fetch('/todos/completed', { method: 'DELETE' });
-    // todos = await res.json();
-    // render();
-  } catch (err) {
-    console.error('[ERROR!!]', err);
-  }
-};
-/* app.delete('/todos/completed', (req, res) => {
-  todos = todos.filter(todo => !todo.completed);
-  res.send(todos);
-}); */
-
-/* // 각각의 리스트 삭제
-$cartItemDel.onclick = ({ target }) => {
-  const parentId = target.closest('li');
-
-  $lists.removeChild(parentId);
-  carts = carts.filter(item => item.id !== +parentId.id.slice(-1));
-}; */
-
-/* // 첫 화면에서 db bookmark 정보 가져와 화면에 렌더링
-(async () => {
-  try {
-    const users = await fetch(`/users/${user.id}`);
-    const { bookmarks } = await users.json();
-    bookmarks.forEach(async movie_id => {
-      // 이 안에서 get 요청을 할 것
-      const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${api_key}&language=ko`;
-      const res = await fetch(url);
-      const results = await res.json();
-      // console.log(results);
-      render(results);
-    });
-  } catch (err) {
-    console.log('[ERROR]', err);
-  }
-})(); */
-
-/* const render = results => {
-  const $li = document.createElement('li');
-  $li.id = results.id;
-  const $a = document.createElement('a');
-  $a.href = '#';
-  $a.textContent = results.title;
-  const $img = document.createElement('img');
-  if (results.poster_path === null) {
-    $img.src = '../image/준비중.png';
+  const cost = +$cost.textContent;
+  if (e.target.matches('.cartDownBtn')) {
+    $number.textContent = number - 1;
   } else {
-    $img.src = `https://image.tmdb.org/t/p/w500/${results.poster_path}`;
+    $number.textContent = number + 1;
   }
-  $a.insertAdjacentElement('afterbegin', $img);
-  $li.appendChild($a);
-  $main__container__movies.appendChild($li);
+
+  const res = await fetch(
+    `http://localhost:4000/api.appPosts/mycart/modify/${cartId}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ numberOfCart: +$number.textContent })
+    }
+  );
+  const res2 = await res.json();
+  console.log(res2, 'res2');
+  return res2;
 };
 
-// <li class='${id}'>
-//   <a href="#">
-//     <img src=""></img>
-//     title
-//   </a>
-// </li>
- */
+$cartItemLists.addEventListener('click', changeCartNumber);
+
+// 각 리스트 삭제
+const cartListDelete = async e => {
+  if (!e.target.matches('.cartDeletItem')) return;
+  const cartId = e.target.closest('li').id;
+  console.log(cartId, 'cart_id');
+
+  const option = {
+    method: 'DELETE'
+  };
+  const res = await fetch(
+    `http://localhost:4000/api/appPosts/mycart/delete/${cartId}`,
+    option
+  );
+  cartItems = await res.json();
+  console.log(cartItems, '삭제후cartItems');
+  $cartItemLists.replaceChild(document(cartItems));
+
+  // 리스트 삭제되면 다시 계산해서 총 합계 나타남.
+  const allCosts = () => {
+    if (!cartItems.length) return '';
+    return (
+      '₩ ' +
+      cartItems.reduce((acc, cur) => acc + cur.price * cur.numberOfCart, 0)
+    );
+  };
+  $totalCost.textContent = allCosts();
+};
+
+$cartItemLists.addEventListener('click', cartListDelete);
+
+// 각 리스트 총 금액
+// const eachListCostTotal = () => {
+//   $cartTotalCost.textContent =
+//     '₩ ' +
+//     cartItems[0].reduce((acc, cur) => acc + cur.price * cur.numberOfCart, 0);
+// };
+
+// $cartItemLists.addEventListener('click', eachListCostTotal());
