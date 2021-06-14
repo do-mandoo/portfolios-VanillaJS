48ad1371907b4449a225294e0aadc4d6ed1ae7d9
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2021-06-14 20:49:45 +0900

    fix:반응형 모바일 햄버거버튼 구현 및 불필요한 마크업 삭제

--- ResponsiveWeb/responsiveweb.js
+++ ResponsiveWeb/responsiveweb.js
@@ -1,1 +1,9 @@
-const sizing = () => {};
+window.addEventListener('click', e => {
+  console.log(e.target, 'e타겟랄라');
+  if (!e.target.matches('.burgerBar') && !e.target.matches('.buttonBurger'))
+    return;
+  const menuToggle = document.querySelector('.mobileView');
+  menuToggle.classList.toggle('isAct');
+  const ulListWrap = document.querySelector('.ulListWrap');
+  ulListWrap.classList.toggle('isAct');
+});
